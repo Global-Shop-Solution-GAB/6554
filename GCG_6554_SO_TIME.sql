d658dc7bcf6995cc758e1849d383250c7a67b7c6
Service Web <sw@gssmail.com>
2024-03-19 09:27:29 -0500

    Commit changes in C:\Websites\Git\emarlow\6554\GCG_6554_SO_TIME.sql

---
+++ GCG_6554_SO_TIME.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_6554_SO_TIME" (
+ "Order_No" CHAR(7),
+ "Record_No" CHAR(4),
+ "Part" CHAR(20),
+ "Description" CHAR(30),
+ "Time_Type" CHAR(1),
+ "O_Hours" DOUBLE,
+ "R_Hours" DOUBLE,
+ "O_Rate" DOUBLE,
+ "R_Rate" DOUBLE,
+ "Total" DOUBLE );
